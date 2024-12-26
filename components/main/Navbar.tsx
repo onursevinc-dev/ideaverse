5994561bc9e6876c0c2102a7ab4fc72ba52bf150
Onur Sevinç <onursevinc.dev@gmail.com>
2024-12-26 16:48:06 +0300

    Name Change

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -6,21 +6,10 @@ const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
-        >
-          <Image
-            src="/NavLogo.png"
-            alt="logo"
-            width={70}
-            height={70}
-            className="cursor-pointer hover:animate-slowspin"
-          />
+        <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
+          <Image src="/NavLogo.png" alt="logo" width={70} height={70} className="cursor-pointer hover:animate-slowspin" />
 
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            WebChain Dev
-          </span>
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">OnurSevinc</span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
@@ -39,13 +28,7 @@ const Navbar = () => {
 
         <div className="flex flex-row gap-5">
           {Socials.map((social) => (
-            <Image
-              src={social.src}
-              alt={social.name}
-              key={social.name}
-              width={24}
-              height={24}
-            />
+            <Image src={social.src} alt={social.name} key={social.name} width={24} height={24} />
           ))}
         </div>
       </div>
