27ab8028f6f0ec6ddaaf1050a47e52493091d568
Onur Sevinç <onursevinc.dev@gmail.com>
2024-12-27 09:12:24 +0300

    change project title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,20 +8,14 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "Ideaverse",
+  description: "This is my portfolio Powered by OnurSevinc",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
-      >
+      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas />
         <Navbar />
         {children}
